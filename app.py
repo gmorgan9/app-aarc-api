66176deb36c7cbe64ed47e339395b821f31ae446
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-26 14:14:00 -0600

    update

--- app.py
+++ app.py
@@ -17,67 +17,40 @@ app.secret_key = secrets.token_hex(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")  # Use your actual database URL
 db = SQLAlchemy(app)
 
-login_manager = LoginManager(app)
-login_manager.login_view = 'https://app-aarc.morganserver.com/'
 
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    work_email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)  # You should hash and salt passwords
+    # Add more fields as needed
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
-protected_urls = ['https://app-aarc.morganserver.com/dashboard/']
 
-def login_required_for_protected(func):
-    @wraps(func)
-    def decorated_view(*args, **kwargs):
-        if not current_user.is_authenticated:
-            flash('You need to be logged in to access this page.', 'error')
-            return redirect(url_for('login', next=request.url))
-        return func(*args, **kwargs)
-    return decorated_view
+login_manager = LoginManager()
+login_manager.login_view = 'https://app-aarc.morganserver.com/'  # Replace 'login' with your login route
+login_manager.init_app(app)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {
-        'message': 'This is JSON data from your Flask API!',
-        'status': 'success'
-    }
-    return jsonify(data)
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    data = request.get_json()
-    work_email = data.get('work_email')
-    password = data.get('password')
+    username = request.json.get('username')
+    password = request.json.get('password')
 
-    user = User.query.filter_by(work_email=work_email).first()
-
-    if user and user.password == password:
+    user = User.query.filter_by(username=username).first()
+    if user and check_password_hash(user.password, password):
         login_user(user)
-        return redirect('https://app-aarc.morganserver.com/dashboard/')
+        return jsonify({'message': 'Login successful'})
     else:
-        return jsonify({'message': 'Invalid username or password', 'status': 'error'})
+        return jsonify({'message': 'Login failed'}), 401
 
-@app.route('/api/check_login', methods=['GET'])
-@login_required
-def check_login():
-    return jsonify({'message': 'You are logged in', 'status': 'success'})
 
 @app.route('/api/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect('https://app-aarc.morganserver.com/')
+    return jsonify({'message': 'Logged out'})
 
-@app.route('/protected')
-@login_required
-def protected():
-    # You can put any protected API logic here
-    return {'message': 'You are logged in and active.'}
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='100.118.102.62', port=5000)
