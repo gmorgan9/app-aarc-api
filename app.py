0b23a63b3705c6684fb05745ac707edb5a4529f6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-26 14:01:48 -0600

    update

--- app.py
+++ app.py
@@ -1,60 +1,44 @@
-from flask import Flask, jsonify, request, redirect, url_for, flash, render_template
-import psycopg2
-import psycopg2.extras
+from flask import Flask, jsonify, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 from flask_cors import CORS
 import secrets
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 from functools import wraps
-import bcrypt  # Add bcrypt for password hashing
 
 load_dotenv()  # Load environment variables from .env
 
 app = Flask(__name__)
 CORS(app)
 app.secret_key = secrets.token_hex(16)
+
+# Configure SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")  # Use your actual database URL
+db = SQLAlchemy(app)
+
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'  # Change to the login route
+login_manager.login_view = 'https://app-aarc.morganserver.com/'
 
-# Database connection parameters
-db_params = {
-    'dbname': os.getenv("DB_NAME"),
-    'user': os.getenv("DB_USER"),
-    'password': os.getenv("DB_PASSWORD"),
-    'host': os.getenv("DB_HOST"),
-    'port': os.getenv("DB_PORT")
-}
-
-def connect_to_database():
-    return psycopg2.connect(**db_params)
-
-class User(UserMixin):
-    def __init__(self, user_id):
-        self.id = user_id
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
 @login_manager.user_loader
 def load_user(user_id):
-    # Load a user from your database by their user_id
-    # Replace this with your database logic
-    with connect_to_database() as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT work_email FROM users WHERE id = %s", (user_id,))
-        user_data = cursor.fetchone()
-        if user_data:
-            return User(user_data[0])
-        else:
-            return None
+    return User.query.get(int(user_id))
 
-def hash_password(password):
-    # Hash the password using bcrypt
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+protected_urls = ['https://app-aarc.morganserver.com/dashboard/']
 
-def check_password(password, hashed_password):
-    # Check if the provided password matches the hashed password
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+def login_required_for_protected(func):
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if not current_user.is_authenticated:
+            flash('You need to be logged in to access this page.', 'error')
+            return redirect(url_for('login', next=request.url))
+        return func(*args, **kwargs)
+    return decorated_view
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
@@ -64,42 +48,36 @@ def get_data():
     }
     return jsonify(data)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/api/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        work_email = request.form['work_email']
-        password = request.form['password']
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
 
-        try:
-            with connect_to_database() as conn:
-                cursor = conn.cursor()
-                cursor.execute("SELECT id, password FROM users WHERE work_email = %s", (work_email,))
-                user_data = cursor.fetchone()
-                
-                if user_data and check_password(password, user_data[1]):
-                    user = User(user_data[0])
-                    login_user(user)
+    user = User.query.filter_by(username=username).first()
 
-                    # Redirect to the dashboard upon successful login
-                    return redirect(url_for('dashboard'))
-                else:
-                    flash('Invalid username or password', 'error')
-        except Exception as e:
-            flash('An error occurred', 'error')
+    if user and user.password == password:
+        login_user(user)
+        return redirect('https://app-aarc.morganserver.com/dashboard/')
+    else:
+        return jsonify({'message': 'Invalid username or password', 'status': 'error'})
 
-    return render_template('login.html')
-
-@app.route('/dashboard')
+@app.route('/api/check_login', methods=['GET'])
 @login_required
-def dashboard():
-    # Protected dashboard route
-    return render_template('dashboard.html')
+def check_login():
+    return jsonify({'message': 'You are logged in', 'status': 'success'})
 
-@app.route('/logout')
+@app.route('/api/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect('https://app-aarc.morganserver.com/')
+
+@app.route('/protected')
+@login_required
+def protected():
+    # You can put any protected API logic here
+    return {'message': 'You are logged in and active.'}
 
 if __name__ == '__main__':
     app.run(debug=True, host='100.118.102.62', port=5000)
