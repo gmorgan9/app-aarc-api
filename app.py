7920feaba10bdad46bb45ae9c3094754d79b66fa
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 02:24:03 -0600

    update

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     work_email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    logged_in = db.Column(db.Boolean, default=False)  # Add a logged_in field
+    logged_in = db.Column(db.Integer, default=0)  # Set logged_in as an integer
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -30,7 +30,7 @@ def login():
 
     if user and bcrypt.check_password_hash(user.password, password):
         # Authentication successful
-        user.logged_in = True  # Set the logged_in status to True
+        user.logged_in = 1  # Set the logged_in status to 1
         db.session.commit()  # Commit the changes to the database
 
         return jsonify({"message": "Login successful"}), 200
