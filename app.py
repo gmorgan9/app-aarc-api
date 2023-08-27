ade9804de17d9aa44dc5a8ad372722cdde8780a8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 02:00:40 -0600

    update

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 from flask_cors import CORS
+from bcrypt import hashpw, gensalt
 
 load_dotenv()  # Load environment variables from .env
 
@@ -17,6 +18,12 @@ class User(db.Model):
     work_email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
+    def set_password(self, password):
+        self.password_hash = hashpw(password.encode('utf-8'), gensalt())
+
+    def check_password(self, password):
+        return hashpw(password.encode('utf-8'), self.password_hash.encode('utf-8')) == self.password_hash.encode('utf-8')
+
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -27,7 +34,7 @@ def login():
     # Check if the user exists
     user = User.query.filter_by(work_email=work_email).first()
 
-    if user and user.password == password:
+    if user and user.check_password(password):
         # Authentication successful
         return jsonify({"message": "Login successful"}), 200
     else:
