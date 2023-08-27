f281337dba96d4bfe6692035ab6f4e23b3dd8b4d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-26 20:41:40 -0600

    update

--- app.py
+++ app.py
@@ -1,87 +1,71 @@
-from flask import Flask, jsonify, request, redirect, url_for
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 from flask_cors import CORS
 import secrets
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-from flask_bcrypt import Bcrypt, check_password_hash
+from flask_bcrypt import Bcrypt
+import jwt
+from functools import wraps
+from datetime import datetime, timedelta
 
 load_dotenv()  # Load environment variables from .env
 
 app = Flask(__name__)
 CORS(app)
-app.secret_key = secrets.token_hex(16)
+app.secret_key = secrets.token_hex(16)  # Generate a new secret key each time the application runs (not recommended for production)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 # Configure SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
-db = SQLAlchemy(app)
 
-# Initialize Bcrypt
-bcrypt = Bcrypt(app)
-
-# Define the User model
-class Users(db.Model, UserMixin):
+# Define your User model
+class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     work_email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
-    def get_id(self):
-        return str(self.user_id)
+# Decorator to ensure the request has a valid token
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
 
-# Initialize Flask-Login
-login_manager = LoginManager()
-login_manager.login_view = 'https://app-aarc.morganserver.com/'  # Replace with your login route
-login_manager.init_app(app)
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 401
 
-# Define the user_loader function
-@login_manager.user_loader
-def load_user(user_id):
-    return db.session.query(Users).get(int(user_id))
+        try:
+            data = jwt.decode(token, app.secret_key, algorithms=['HS256'])
+            current_user = User.query.get(data['user_id'])
+        except:
+            return jsonify({'message': 'Token is invalid'}), 401
 
-# Your '/dashboard' route
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    # Your dashboard logic here
-    return redirect('https://app-aarc.morganserver.com/dashboard')
+        return f(current_user, *args, **kwargs)
+
+    return decorated
 
 
-# login
+# User login
 @app.route('/api/login', methods=['POST'])
 def login():
-    work_email = request.form.get('work_email')
-    password = request.form.get('password')
+    work_email = request.json.get('work_email')
+    password = request.json.get('password')
 
-    user = Users.query.filter_by(work_email=work_email).first()
-    if user and check_password_hash(user.password, password):
-        # Password matches; you can proceed with login
-        login_user(user)  # Log the user in
-        return redirect(url_for('dashboard'))  # Redirect to the dashboard
+    user = User.query.filter_by(work_email=work_email).first()
+
+    if user and bcrypt.check_password_hash(user.password, password):
+        token = jwt.encode({'user_id': user.user_id, 'exp': datetime.utcnow() + timedelta(hours=1)},
+                           app.secret_key, algorithm='HS256')
+        return jsonify({'token': token.decode('utf-8')}), 200
     else:
-        # Password doesn't match or user doesn't exist
         return jsonify({'message': 'Login failed'}), 401
-    
 
-# Add this route to your Flask app
-@app.route('/api/user')
-@login_required
-def get_user_info():
-    # Access the current user and return their information
-    user_info = {
-        "work_email": current_user.work_email,
-        # Add other user attributes here if needed
-    }
-    return jsonify(user_info)
-
-
-
-# Your '/api/logout' route
-@app.route('/api/logout')
-@login_required
-def logout():
-    logout_user()
-    return jsonify({'message': 'Logged out'})
+# Protected route
+@app.route('/api/protected', methods=['GET'])
+@token_required
+def protected(current_user):
+    return jsonify({'message': 'This is a protected route'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='100.118.102.62', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
