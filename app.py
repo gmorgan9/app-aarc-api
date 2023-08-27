c27587c9cb6bbc41719a6a93fdf829b6892aacbb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 02:02:01 -0600

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 from flask_cors import CORS
-from bcrypt import hashpw, gensalt
+from flask_bcrypt import Bcrypt
 
 load_dotenv()  # Load environment variables from .env
 
@@ -11,19 +11,13 @@ app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")  # Load SQLAlchemy URI from .env
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 class User(db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True)
     work_email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-
-    def set_password(self, password):
-        self.password_hash = hashpw(password.encode('utf-8'), gensalt())
-
-    def check_password(self, password):
-        return hashpw(password.encode('utf-8'), self.password_hash.encode('utf-8')) == self.password_hash.encode('utf-8')
-
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -34,13 +28,12 @@ def login():
     # Check if the user exists
     user = User.query.filter_by(work_email=work_email).first()
 
-    if user and user.check_password(password):
+    if user and bcrypt.check_password_hash(user.password, password):
         # Authentication successful
         return jsonify({"message": "Login successful"}), 200
     else:
         # Authentication failed
         return jsonify({"message": "Login failed"}), 401
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='100.118.102.62', port=5000)
