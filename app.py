ebc4f9bc688e59284e3558ef296f271c14465d37
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 01:35:51 -0600

    update

--- app.py
+++ app.py
@@ -3,17 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 from flask_cors import CORS
-import secrets
 from flask_bcrypt import Bcrypt
-import jwt
-from functools import wraps
 from datetime import datetime, timedelta
 
 load_dotenv()  # Load environment variables from .env
 
 app = Flask(__name__)
 CORS(app)
-app.secret_key = secrets.token_hex(16)  # Generate a new secret key each time the application runs (not recommended for production)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")  # Load SQLAlchemy URI from .env
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -21,53 +17,36 @@ bcrypt = Bcrypt(app)
 # Configure SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 
-# Define your User model
 class User(db.Model):
-    __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True)
     work_email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
-# Decorator to ensure the request has a valid token
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.headers.get('Authorization')
-
-        if not token:
-            return jsonify({'message': 'Token is missing'}), 401
-
-        try:
-            data = jwt.decode(token, app.secret_key, algorithms=['HS256'])
-            current_user = User.query.get(data['user_id'])
-        except:
-            return jsonify({'message': 'Token is invalid'}), 401
-
-        return f(current_user, *args, **kwargs)
-
-    return decorated
+    def __init__(self, work_email, password):
+        self.work_email = work_email
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
 
-# User login
-@app.route('/api/login', methods=['POST'])
+
+@app.route('/login', methods=['POST'])
 def login():
-    work_email = request.json.get('work_email')
-    password = request.json.get('password')
+    data = request.get_json()
 
-    user = User.query.filter_by(work_email=work_email).first()
+    # Check if the request contains a username and password
+    if 'work_email' not in data or 'password' not in data:
+        return jsonify({'message': 'Missing work_email or password'}), 400
 
-    if user and bcrypt.check_password_hash(user.password, password):
-        token = jwt.encode({'user_id': user.user_id, 'exp': datetime.utcnow() + timedelta(hours=1)},
-                           app.secret_key, algorithm='HS256')
-        return jsonify({'token': token}), 200  # Removed .decode('utf-8')
+    # Try to find a user with the provided username
+    user = User.query.filter_by(work_email=data['work_email']).first()
+
+    if user and bcrypt.check_password_hash(user.password, data['password']):
+        # Password is correct, generate a token or session here if needed
+        return jsonify({'message': 'Login successful'})
     else:
-        return jsonify({'message': 'Login failed'}), 401
+        return jsonify({'message': 'Invalid username or password'}), 401
 
-# Protected route
-@app.route('/api/protected', methods=['GET'])
-@token_required
-def protected(current_user):
-    return jsonify({'message': 'This is a protected route'}), 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='100.118.102.62', port=5000)
