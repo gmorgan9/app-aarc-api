a6036a3154c5cb6c3781faa2100c8b93cb68392f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 19:23:53 -0600

    update

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ import psycopg2
 from dotenv import load_dotenv
 from flask import Flask
 
+GET_USERS = """SELECT * FROM users;"""
+
 load_dotenv()
 
 app = Flask(__name__)
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
 
 @app.get("/")
 def home():
-    return "hello world"
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(GET_USERS)
+            users = cursor.fetchone()[0]
+    return {users}
 
 if __name__ == '__main__':
     app.run(debug=True, host='100.118.102.62', port=5000)
