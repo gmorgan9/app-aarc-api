784032c5a9ddeb29199623c43a3beefbec7a5bc8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 23:47:28 -0600

    update

--- app.py
+++ app.py
@@ -5,45 +5,6 @@ from flask import Flask, request, jsonify, session
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from flask_session import Session
-
-# Define your SQL query to check login credentials
-CHECK_LOGIN = """SELECT user_id, work_email, password FROM users WHERE work_email = %s;"""
-
-load_dotenv()
-
-app = Flask(__name__)
-app.secret_key = os.urandom(24)  # Set a secret key for session security
-bcrypt = Bcrypt(app)
-
-url = os.getenv("DATABASE_URL")
-connection = psycopg2.connect(url)
-login_manager = LoginManager(app)
-app.config['SESSION_TYPE'] = 'filesystem'  # Use filesystem to store sessions
-Session(app)
-
-# User model for Flask-Login
-class User(UserMixin):
-    def __init__(self, user_id, work_email):
-        self.id = user_id
-        self.work_email = work_email
-
-# Configure Flask-Login to load users
-@login_manager.user_loader
-def load_user(user_id):
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT user_id, work_email FROM users WHERE user_id = %s;", (user_id,))
-            user_data = cursor.fetchone()
-            if user_data:
-                return User(user_data[0], user_data[1])
-
-import os
-import psycopg2
-from dotenv import load_dotenv
-from flask import Flask, request, jsonify, session
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
-from flask_session import Session
 from flask_cors import CORS
 
 # Define your SQL query to check login credentials
