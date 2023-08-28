19ea23865109f1254111d0cf68145b9dbd0bcecd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 13:19:22 -0600

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, session
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_bcrypt import Bcrypt
 import os
