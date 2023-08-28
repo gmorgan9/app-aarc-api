2d1f460844b79a5a29b767f11e0ad4c493bcd394
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 19:25:28 -0600

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, jsonify
 
 GET_USERS = """SELECT * FROM users;"""
 
@@ -17,7 +17,7 @@ def home():
         with connection.cursor() as cursor:
             cursor.execute(GET_USERS)
             users = cursor.fetchone()[0]
-    return {users}
+    return jsonify({"users": users})
 
 if __name__ == '__main__':
     app.run(debug=True, host='100.118.102.62', port=5000)
