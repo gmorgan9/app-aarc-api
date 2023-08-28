f6b42d22f140d6f50f61860c544aa271500ba291
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-28 10:00:28 -0600

    update

--- app.py
+++ app.py
@@ -34,8 +34,13 @@ def token_required(f):
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
             current_user = data['username']
-        except:
-            return jsonify({'message': 'Token is invalid!'}), 401
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'message': 'Invalid token'}), 401
+        except Exception as e:
+            print(f"Error decoding token: {e}")
+            return jsonify({'message': 'Token decoding error'}), 401
 
         return f(current_user, *args, **kwargs)
 
